030885d5df7f1ed27d3611962a1704cb0d6cad6c
k_eryomenko <k_eryomenko@wargaming.net>
2024-09-21 17:24:26 +0300

    Implement work with several files

--- project8/vm_translator.py
+++ project8/vm_translator.py
@@ -1,3 +1,4 @@
+import pathlib
 import sys
 from pathlib import Path
 
@@ -11,33 +12,41 @@ ASM_EXTENSION = '.asm'
 
 def main():
     if len(sys.argv) < 2:
-        print(f'path to *{VM_EXTENSION} must be sent')
+        print(f'path to program must be provided')
         sys.exit(1)
     path_to_program = sys.argv[1]
-    if not path_to_program.endswith(VM_EXTENSION):
-        print(f'Program name must have extension {VM_EXTENSION}')
-        sys.exit(1)
-
     input_path = Path(path_to_program)
-    with input_path.open() as input_file:
-        program = input_file.read().split('\n')
+    program_name = input_path.name
+    if input_path.is_file():
+        if not path_to_program.endswith(VM_EXTENSION):
+            print(f'Program name must have extension {VM_EXTENSION}')
+            sys.exit(1)
+        paths = [input_path]
+        output_path = input_path.with_suffix(ASM_EXTENSION)
 
-    program = [line.strip() for line in program]
-    program = skip_comments(program)
-    program = skip_empty_lines(program)
+    else:
+        paths = list(input_path.glob('*.vm'))
+        output_path = input_path / f'{program_name}.asm'
 
-    parser = Parser(program)
-    code_writer = CodeWriter(filename=input_path.name)
+    code_writer = CodeWriter()
 
-    while parser.has_more_lines():
-        parser.advance()
-        command_type = parser.command_type()
-        arg1 = parser.arg1
-        arg2 = parser.arg2
-        code_writer.translate(parser.command, command_type, arg1, arg2)
+    for path in paths:
+        with path.open() as input_file:
+            program = input_file.read().split('\n')
+            program = [line.strip() for line in program]
+            program = skip_comments(program)
+            program = skip_empty_lines(program)
+            parser = Parser(program)
+            code_writer.set_filename(path.name)
+            while parser.has_more_lines():
+                parser.advance()
+                command_type = parser.command_type()
+                arg1 = parser.arg1
+                arg2 = parser.arg2
+                code_writer.translate(parser.command, command_type, arg1, arg2)
     code_writer.close()
 
-    with input_path.with_suffix(ASM_EXTENSION).open('w') as output_file:
+    with output_path.open('w') as output_file:
         program_bin_content = '\r\n'.join(code_writer.asm_program)
         output_file.write(program_bin_content)
 
